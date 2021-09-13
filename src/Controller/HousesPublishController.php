52abd16e88102ee2312421ba10ea20e9a1df8d3e
Hifumisama <m.khayat92@gmail.com>
2021-09-13 12:58:55 +0200

    [WIP]: tentative de trouver un moyen efficace de filtrer les données de la base de données

--- src/Controller/HousesPublishController.php
+++ src/Controller/HousesPublishController.php
@@ -1,23 +1,19 @@
 <?php
 
-
 namespace App\Controller;
 
 use App\Entity\Houses;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 
-
 #[AsController]
-class HousesPublishController extends AbstractController{
+class HousesPublishController extends AbstractController
+{
 
 /*Affiche la nouvelle requete dans l'API*/
-
-
     public function __invoke(Houses $data): Houses
     {
-        $this->HousesPublishController->handle($data);
-
+        echo ($data);
         return $data;
     }
 }
--- src/Entity/Houses.php
+++ src/Entity/Houses.php
@@ -2,344 +2,357 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource(
+
+    itemOperations:[
+        'put',
+        'delete',
+        'patch',
+        'get' => ['normalization_context' => ['groups' => ['read:collection', 'read:item', 'read:Post']]],
+        'get' => ['path' => '/houses/{id}', 'requirements' => ['id' => '\d+']],
+    ],
+
+),
+]
 /**
  * Houses
  *
  * @ORM\Table(name="houses", indexes={@ORM\Index(name="house_id_category", columns={"ID_category"}), @ORM\Index(name="house_id_user", columns={"ID_user"})})
  * @ORM\Entity
+ * @ApiFilter(SearchFilter::class, properties={"city"}))
+ * @ApiFilter(DateFilter::class, properties={"dateFin"}))
+ * @ApiFilter(RangeFilter::class, properties={"nbbeds"}))
  */
-
-/*Permet de creer des operations personnalisé (requete)*/
-#[ApiResource(
-
-    itemOperations: [
-    'put',
-    'delete',
-    'patch',
-    'get' => ['normalization_context' => ['groups' => ['read:collection', 'read:item', 'read:Post']]],
-    'get'=> ['path' => '/houses/{id}', 'requirements' => ['id' => '\d+']],
-
-        /*New Items*/
-        'search'=>[
-            'method' => 'POST',
-            'path' => '/houses/search',
-            'controller' => HousesPublishController::class,
-            /*'openapi_context' =>[
-                'summary'=> 'permet de faire search',
-                'requestBody'=>[
-                    'content'=>[
-                        'application/json' => [
-                            'schema' =>[]
-                        ]
-                    ]
-                ]
-            ]*/
-
-        ]
-]
-
-),
-]
-
-class Houses
+    class Houses
 {
-    #[Groups(['search'])]
+        #[Groups(['search'])]
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="ID", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     *
-     */
+/**
+ * @var int
+ *
+ * @ORM\Column(name="ID", type="integer", nullable=false)
+ * @ORM\Id
+ * @ORM\GeneratedValue(strategy="IDENTITY")
+ *
+ */
 
     private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="title", type="string", length=50, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="title", type="string", length=50, nullable=false)
+ */
     private $title;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="description", type="text", length=65535, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="description", type="text", length=65535, nullable=false)
+ */
     private $description;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="address", type="string", length=255, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="address", type="string", length=255, nullable=false)
+ */
     private $address;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="zipcode", type="integer", nullable=false)
-     */
+/**
+ * @var int
+ *
+ * @ORM\Column(name="zipcode", type="integer", nullable=false)
+ */
     private $zipcode;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="city", type="string", length=255, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="city", type="string", length=255, nullable=false)
+ */
     private $city;
 
-    /**
-     * @var bool
-     *
-     * @ORM\Column(name="status", type="boolean", nullable=false)
-     */
+/**
+ * @var bool
+ *
+ * @ORM\Column(name="status", type="boolean", nullable=false)
+ */
     private $status;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="nbBeds", type="integer", nullable=false)
-     */
+/**
+ * @var int
+ *
+ * @ORM\Column(name="nbBeds", type="integer", nullable=false)
+ */
     private $nbbeds;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="price", type="integer", nullable=false)
-     */
+/**
+ * @var int
+ *
+ * @ORM\Column(name="price", type="integer", nullable=false)
+ */
     private $price;
 
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="tax", type="integer", nullable=false)
-     */
+/**
+ * @var int
+ *
+ * @ORM\Column(name="tax", type="integer", nullable=false)
+ */
     private $tax;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="listID_activities", type="string", length=255, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="listID_activities", type="string", length=255, nullable=false)
+ */
     private $listidActivities;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="listID_tags", type="string", length=255, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="listID_tags", type="string", length=255, nullable=false)
+ */
     private $listidTags;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="listID_pics", type="string", length=255, nullable=false)
-     */
+/**
+ * @var string
+ *
+ * @ORM\Column(name="listID_pics", type="string", length=255, nullable=false)
+ */
     private $listidPics;
 
-    /**
-     * @var \Categories
-     *
-     * @ORM\ManyToOne(targetEntity="Categories")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="ID_category", referencedColumnName="ID")
-     * })
-     */
+/**
+ * @var \Categories
+ *
+ * @ORM\ManyToOne(targetEntity="Categories")
+ * @ORM\JoinColumns({
+ *   @ORM\JoinColumn(name="ID_category", referencedColumnName="ID")
+ * })
+ */
     private $idCategory;
 
-    /**
-     * @var \User
-     *
-     * @ORM\ManyToOne(targetEntity="User")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="ID_user", referencedColumnName="ID")
-     * })
-     */
+/**
+ * @var \User
+ *
+ * @ORM\ManyToOne(targetEntity="User")
+ * @ORM\JoinColumns({
+ *   @ORM\JoinColumn(name="ID_user", referencedColumnName="ID")
+ * })
+ */
     private $idUser;
 
+/**
+ * @ORM\Column(type="datetime", nullable=true)
+ */
+    private $dateDebut;
 
-    public function getId(): ?int
+/**
+ * @ORM\Column(type="datetime", nullable=true)
+ */
+    private $dateFin;
+
+    function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTitle(): ?string
+    function getTitle(): ?string
     {
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    function setTitle(string $title): self
     {
         $this->title = $title;
 
         return $this;
     }
 
-    public function getDescription(): ?string
+    function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    function setDescription(string $description): self
     {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getAddress(): ?string
+    function getAddress(): ?string
     {
         return $this->address;
     }
 
-    public function setAddress(string $address): self
+    function setAddress(string $address): self
     {
         $this->address = $address;
 
         return $this;
     }
 
-    public function getZipcode(): ?int
+    function getZipcode(): ?int
     {
         return $this->zipcode;
     }
 
-    public function setZipcode(int $zipcode): self
+    function setZipcode(int $zipcode): self
     {
         $this->zipcode = $zipcode;
 
         return $this;
     }
 
-    public function getCity(): ?string
+    function getCity(): ?string
     {
         return $this->city;
     }
 
-    public function setCity(string $city): self
+    function setCity(string $city): self
     {
         $this->city = $city;
 
         return $this;
     }
 
-    public function getStatus(): ?bool
+    function getStatus(): ?bool
     {
         return $this->status;
     }
 
-    public function setStatus(bool $status): self
+    function setStatus(bool $status): self
     {
         $this->status = $status;
 
         return $this;
     }
 
-    public function getNbbeds(): ?int
+    function getNbbeds(): ?int
     {
         return $this->nbbeds;
     }
 
-    public function setNbbeds(int $nbbeds): self
+    function setNbbeds(int $nbbeds): self
     {
         $this->nbbeds = $nbbeds;
 
         return $this;
     }
 
-    public function getPrice(): ?int
+    function getPrice(): ?int
     {
         return $this->price;
     }
 
-    public function setPrice(int $price): self
+    function setPrice(int $price): self
     {
         $this->price = $price;
 
         return $this;
     }
 
-    public function getTax(): ?int
+    function getTax(): ?int
     {
         return $this->tax;
     }
 
-    public function setTax(int $tax): self
+    function setTax(int $tax): self
     {
         $this->tax = $tax;
 
         return $this;
     }
 
-    public function getListidActivities(): ?string
+    function getListidActivities(): ?string
     {
         return $this->listidActivities;
     }
 
-    public function setListidActivities(string $listidActivities): self
+    function setListidActivities(string $listidActivities): self
     {
         $this->listidActivities = $listidActivities;
 
         return $this;
     }
 
-    public function getListidTags(): ?string
+    function getListidTags(): ?string
     {
         return $this->listidTags;
     }
 
-    public function setListidTags(string $listidTags): self
+    function setListidTags(string $listidTags): self
     {
         $this->listidTags = $listidTags;
 
         return $this;
     }
 
-    public function getListidPics(): ?string
+    function getListidPics(): ?string
     {
         return $this->listidPics;
     }
 
-    public function setListidPics(string $listidPics): self
+    function setListidPics(string $listidPics): self
     {
         $this->listidPics = $listidPics;
 
         return $this;
     }
 
-    public function getIdCategory(): ?Categories
+    function getIdCategory(): ?Categories
     {
         return $this->idCategory;
     }
 
-    public function setIdCategory(?Categories $idCategory): self
+    function setIdCategory(?Categories $idCategory): self
     {
         $this->idCategory = $idCategory;
 
         return $this;
     }
 
-    public function getIdUser(): ?User
+    function getIdUser(): ?User
     {
         return $this->idUser;
     }
 
-    public function setIdUser(?User $idUser): self
+    function setIdUser(?User $idUser): self
     {
         $this->idUser = $idUser;
 
         return $this;
     }
 
+    function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
 
+    function setDateDebut(?\DateTimeInterface$dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
 
+        return $this;
+    }
 
+    function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
 
+    function setDateFin(?\DateTimeInterface$dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
 
 }
